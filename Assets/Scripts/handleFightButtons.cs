4aee2d1bd419d72a0876666576395195c1f993c8
Twojcik3 <tomek.wojciik12@wp.pl>
2020-11-28 17:58:56 +0100

    Add 3 stage buttons

---
+++ Assets/Scripts/handleFightButtons.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class handleFightButtons : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void PlayGame_Fight1()
+    {
+        SceneManager.LoadScene(2);
+    }
+    public void PlayGame_Fight2()
+    {
+        SceneManager.LoadScene(3);
+    }
+    public void PlayGame_Fight3()
+    {
+        SceneManager.LoadScene(3);
+    }
+   
+}
