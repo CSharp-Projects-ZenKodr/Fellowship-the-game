7d1faa8620c4c9636cc9f19982dc4a0ca129f0d6
szym-sicinski <szym.sicinski@gmail.com>
2020-12-01 12:59:08 +0100

    Back button on Map

--- Assets/Scripts/handleFightButtons.cs
+++ Assets/Scripts/handleFightButtons.cs
@@ -5,16 +5,9 @@ using UnityEngine.SceneManagement;
 
 public class handleFightButtons : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void BackButton()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SceneManager.LoadScene(0);
     }
     public void PlayGame_Fight1()
     {
